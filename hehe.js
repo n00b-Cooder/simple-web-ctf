435ddfe0f34b8ddd07c08a173fba379b91250480
fake <84218755+n00b-Cooder@users.noreply.github.com>
2021-08-18 14:07:17 +0530

    Update hehe.js

--- hehe.js
+++ hehe.js
@@ -17,5 +17,5 @@ function openTab(tabName,elmnt,color) {
 window.onload = function() {
     openTab('tabintro', this, '#222');
 }
-/*So basically this ctf has four flags like eg:-THe-KHC1{******} , THe-KHC2{******} , THe-KHC3{******}..ect  and the last flag will be like THe-KHC3{******}*/
+/*So basically this ctf has four flags like eg:-THe-KHC1{******} , THe-KHC2{******}...ect  and the last flag will be like THe-KHC3{******}*/
 /*so here is your second flag THe-KHC2{FLEG|2} */
